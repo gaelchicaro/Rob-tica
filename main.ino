47755976a4455b64218d90acfb7876b217359c26
gaelchicaro <gaelchicaro@gmail.com>
2025-11-10 14:15:03 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -23,6 +23,10 @@ int valor_analogico;
 Adafruit_BMP280 bmp;
 
 //motores
+// Controle de dois motores DC com L298N e Bluetooth (HC-05)
+// Autor: ChatGPT - GPT-5
+
+// Pinos do Motor A (Frente/Trás)
 const int IN1 = 8;
 const int IN2 = 9;
 const int ENA = 5;
@@ -34,73 +38,7 @@ const int ENB = 6;
 
 char comando; // Variável para armazenar o comando recebido via Bluetooth
 
-void setup() 
-{
-  Serial.begin(9600);
-  Serial.println("DHTxx test!");
-  //dht11
-  dht.begin();
-  //higrômetro
-  pinMode(pino_sinal_analogico, INPUT);
-
-  //motores
- pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
-
-  // Inicializa comunicação serial com o módulo Bluetooth
-  Serial.begin(9600);
-
-  // Motores desligados inicialmente
-  parar();
-
-  //BMP280
-  Serial.begin(9600);
-  while ( !Serial ) delay(100);   
-  Serial.println(F("BMP280 test"));
-  unsigned status;
-  status = bmp.begin(0x76);
-  if (!status) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                      "try a different address!"));
-    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
-    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-    Serial.print("        ID of 0x60 represents a BME 280.\n");
-    Serial.print("        ID of 0x61 represents a BME 680.\n");
-    while (1) delay(10);
-  }
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-}
- 
-void loop() 
-{
-if (Serial.available()) {
-    comando = Serial.read();
-    Serial.println(comando); // Para depuração, mostra o comando recebido
-
-    switch (comando) {
-      case 'F': frente(); break;
-      case 'B': tras(); break;
-      case 'L': esquerda(); break;
-      case 'R': direita(); break;
-      case 'S': parar(); break;
-      default: parar(); break;
-    }
-  }
-}
-  bmp280();
-  higrometro();
-  DHt11();
-}
-
+//funções
 void bmp280(){
     Serial.print(F("Pressure = "));
     Serial.print(bmp.readPressure());
@@ -197,5 +135,70 @@ void parar() {
   digitalWrite(IN4, LOW);
 }  
 
-}
 
+void setup() 
+{
+  //dht11
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  dht.begin();
+
+  //higrômetro
+  pinMode(pino_sinal_analogico, INPUT);
+
+  //BMP280
+  Serial.begin(9600);
+  while ( !Serial ) delay(100);   
+  Serial.println(F("BMP280 test"));
+  unsigned status;
+  status = bmp.begin(0x76);
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+  }
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     
+                  Adafruit_BMP280::SAMPLING_X2,     
+                  Adafruit_BMP280::SAMPLING_X16,    
+                  Adafruit_BMP280::FILTER_X16,  
+                  Adafruit_BMP280::STANDBY_MS_500); 
+  
+  //motores
+ // Configuração dos pinos de saída
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  // Inicializa comunicação serial com o módulo Bluetooth
+  Serial.begin(9600);
+
+  // Motores desligados inicialmente
+  parar();
+}
+ 
+void loop() 
+{
+  if (Serial.available()) {
+    comando = Serial.read();
+    Serial.println(comando); // Para depuração, mostra o comando recebido
+
+    switch (comando) {
+      case 'F': frente(); break;
+      case 'B': tras(); break;
+      case 'L': esquerda(); break;
+      case 'R': direita(); break;
+      case 'S': parar(); break;
+      default: parar(); break;
+    }
+  }
+  bmp280();
+  higrometro();
+  DHt11();
